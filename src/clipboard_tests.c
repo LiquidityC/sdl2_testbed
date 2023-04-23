67eae6d34fcdef11dd404afea0090f15bd84850e
Linus Probert <linus.probert@gmail.com>
2023-04-24 00:02:54 +0200

    More clipboard testbed

--- src/clipboard_tests.c
+++ src/clipboard_tests.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #include "SDL.h"
+#include "SDL_clipboard.h"
 #include "SDL_events.h"
+#include "SDL_oldnames.h"
+#include "SDL_pixels.h"
+#include "SDL_render.h"
+#include "SDL_rwops.h"
+#include "SDL_surface.h"
+#include "SDL_video.h"
+
+#define BUF_SIZE 1024*1024
+
+static SDL_Texture *texture_from_clipboard(SDL_Renderer *renderer, SDL_FRect *size)
+{
+	size_t length;
+	void *data = SDL_GetClipboardData(&length, "image/bmp");
+	SDL_RWops *rw;
+	SDL_Surface *surface;
+	SDL_Texture *texture;
+
+	rw = SDL_RWFromMem(data, length);
+	surface = SDL_LoadBMP_RW(rw, 1);
+	size->w = surface->w;
+	size->h = surface->h;
+	texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+	SDL_DestroySurface(surface);
+	SDL_free(data);
+
+	return texture;
+}
 
 int main(int argc, char **argv)
 {
@@ -15,16 +45,13 @@ int main(int argc, char **argv)
 		goto out;
 	}
 
-	SDL_SetEventEnabled(SDL_EVENT_CLIPBOARD_DATA_UPDATE, SDL_TRUE);
-
-	window = SDL_CreateWindow("Testbed", 800, 600, 0);
+	SDL_CreateWindowAndRenderer(800, 600, 0, &window, &renderer);
 	if (!window) {
 		printf("Err: %s\n", SDL_GetError());
 		result = -1;
 		goto clean;
 	}
 
-	renderer = SDL_CreateRenderer(window, 0, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if (!renderer) {
 		printf("Err: %s\n", SDL_GetError());
 		result = -1;
@@ -34,7 +61,11 @@ int main(int argc, char **argv)
 	/* Main loop */
 	SDL_Event e;
 	SDL_bool running = SDL_TRUE;
+	SDL_Texture *texture = NULL;
+	SDL_FRect size = { 0, 0, 0, 0 };
 	while (running) {
+		bool update = false;
+		bool clipboard = false;
 		while (SDL_PollEvent(&e)) {
 			switch (e.type) {
 				case SDL_EVENT_QUIT:
@@ -42,25 +73,67 @@ int main(int argc, char **argv)
 					break;
 				case SDL_EVENT_CLIPBOARD_UPDATE:
 					puts("Clipboard update");
+					update = true;
 					break;
-				case SDL_EVENT_CLIPBOARD_DATA_UPDATE:
-					puts("Clipboard data update");
-					printf("Event: %s\n", e.clipboard.mime_type);
-					SDL_free(e.clipboard.mime_type);
+				case SDL_EVENT_KEY_DOWN:
+					if (e.key.keysym.sym == SDLK_c)
+						clipboard = true;
 					break;
 			}
 		}
 
-		if (SDL_HasClipboardData("image/png")) {
-			printf("[%lu]: Clipboard contains image/png\n", SDL_GetTicks());
-		} else {
-			printf("[%lu]: Clipboard does not contain image/png\n", SDL_GetTicks());
-		}
-		printf("SDL_Event size: %zu\n", sizeof(SDL_Event));
-
 		SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255);
+		SDL_RenderClear(renderer);
+
+		if (update) {
+			if (SDL_HasClipboardData("image/bmp")) {
+				printf("[%lu]: Clipboard contains image/png\n", SDL_GetTicks());
+				if (texture != NULL) {
+					SDL_DestroyTexture(texture);
+					texture = NULL;
+				}
+				texture = texture_from_clipboard(renderer, &size);
+			}
+		}
+
+		if (clipboard) {
+			printf("Pushing to clipboard\n");
+
+			Uint8 bmp_data[BUF_SIZE];
+			Uint8 jpg_data[BUF_SIZE];
+			size_t jpg_size= 0, bmp_size = 0;
+			FILE *fp;
+
+			fp = fopen("../assets/small_panda.bmp", "rb");
+			if (fp != NULL) {
+				printf("Including bmp\n");
+				bmp_size = fread(bmp_data, 1, BUF_SIZE, fp);
+				fclose(fp);
+			}
+			fp = fopen("../assets/small_panda.jpeg", "rb");
+			if (fp != NULL) {
+				printf("Including jpeg\n");
+				jpg_size = fread(jpg_data, 1, BUF_SIZE, fp);
+				fclose(fp);
+			}
+
+			SDL_ClipboardData *cb = SDL_CreateClipboardData(bmp_data, bmp_size, "image/bmp");
+			SDL_AddClipboardData(cb, jpg_data, jpg_size, "image/jpeg");
+
+			if (SDL_SetClipboardData(cb, SDL_TRUE) != 0) {
+				printf("ERR: %s\n", SDL_GetError());
+			}
+		}
+
+		if (texture != NULL) {
+			SDL_RenderTexture(renderer, texture, NULL, &size);
+		}
+
+		SDL_FRect r = { 20, 20, 760, 560 };
+		SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+		SDL_RenderRect(renderer, &r);
+
 		SDL_RenderPresent(renderer);
-		SDL_Delay(1000);
 	}
 
 clean:
